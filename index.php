8a38245e8c1b80de1c7c6af81057e0274fe1285a
mike mambwe <mikeapple056@gmail.com>
2023-12-28 18:01:02 +0200

    model that needs to be fix for the form to work

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
    <?php include('db_connection.php'); ?>
 
    <div class="box1">
-   <h2>All Employees</h2>
-   <button class="btn btn-primary">Add Employee</button>
-    </div>
+       <h2>All Employees</h2>
+       <button class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Add Employee</button>
+   </div>
    <table class="table table-hover table-bordered table-str">
        <thead>
            <tr>
@@ -40,4 +40,25 @@
 
        </tbody>
    </table>
+
+   <!-- Modal -->
+   <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+       <div class="modal-dialog" role="document">
+           <div class="modal-content">
+               <div class="modal-header">
+                   <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                   <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                       <span aria-hidden="true">&times;</span>
+                   </button>
+               </div>
+               <div class="modal-body">
+                   ...
+               </div>
+               <div class="modal-footer">
+                   <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                   <button type="button" class="btn btn-primary">Save changes</button>
+               </div>
+           </div>
+       </div>
+   </div>
    <?php include('footer.php'); ?>
