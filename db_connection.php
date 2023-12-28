afb20ea8ce9b78610aa8e4320a99a97b9876696a
mike mambwe <mikeapple056@gmail.com>
2023-12-28 10:16:46 +0200

    The database connection

--- db_connection.php
+++ db_connection.php
@@ -1,4 +1,18 @@
 <?php
 //databse connection file
+define("HOSTNAME",'localhost');
+define("USERNAME",'root');
+define("PASSWORD",'');
+define("DATABASE",'crud_operations');
+
+//connection
+$connection = mysqli_connect(HOSTNAME,USERNAME,PASSWORD,DATABASE);
+
+//check the connection
+if(!$connection){
+    die("Connection Failed!");
+}else{
+    echo "Connected!";
+}
 
 ?>
